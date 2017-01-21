98cf9f679b45f36219dca2264fa29c901cca37a0
Seva Gavrilov <gavrilovseva@gmail.com>
2017-01-21 17:45:04 -0300

    Added multiple patterns, and a way of replacing them.

--- ggj/model/objectives.py
+++ ggj/model/objectives.py
@@ -28,10 +28,36 @@ class PatternObjective(Objective):
         m = self.model.scene.table.matrix
         return random.sample([x.color for row in m for x in row], size)
 
-    def check_conditions(self):
-        balls = [x.color for x in self.model.tube.balls if x]
+    def check_conditions(self, tube):
+        balls = [x.color for x in tube.balls if x]
         n = len(self.pattern)
-        return any((self.pattern == balls[i:i+n]) for i in range(len(balls) - n + 1))
+        return self.pattern == balls[:n]
 
     def __str__(self):
         return str(list(reversed(self.pattern)))
+
+
+class PatternObjectivesGenerator(object):
+
+    def __init__(self, model, number, length):
+        self.model, self.number, self.length = model, number, length
+
+        self.objectives = []
+        total_size = number * length
+        self.m = self.model.scene.table.matrix
+        pattern = random.sample([x.color for row in self.m for x in row], total_size)
+
+        for i in range(0, total_size, length):
+            objective = PatternObjective(model, pattern[i:min(i + length, total_size)])
+            self.objectives.append(objective)
+
+    def replace_objective(self, objective):
+        self.objectives.remove(objective)
+        balls = [x.color for row in self.m for x in row]
+        for color in [x for obj in self.objectives for x in obj.pattern]:
+            try:
+                balls.remove(color)
+            except ValueError:
+                pass
+        new_obj = PatternObjective(self.model, random.sample(balls, self.length))
+        self.objectives.append(new_obj)
