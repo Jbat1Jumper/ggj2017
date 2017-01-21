cc39089cf8ed6ebe6b86d5e26ca2ebec679c237e
Nikita Zdanovitch <nzdanovitch@gmail.com>
2017-01-21 13:24:44 -0300

    extracted vec class to another file

--- pyxit/entity.py
+++ pyxit/entity.py
@@ -1,10 +1,4 @@
-
-
-class Vec(object):
-
-    def __init__(self, x=None, y=None):
-        self.x = float(x) or 0.0
-        self.y = float(y) or 0.0
+from pyxit.vec import Vec
 
 
 class Entity(object):
