13f8ce7988ffdaf17f9a42fcfc434d83283b51fd
Nikita Zdanovitch <nzdanovitch@gmail.com>
2017-01-21 16:25:38 -0300

    render balls

--- ggj/entities/ball.py
+++ ggj/entities/ball.py
@@ -1,4 +1,5 @@
 from pyxit.entity import Entity
+import pygame as pg
 
 
 class Ball(Entity):
@@ -6,3 +7,14 @@ class Ball(Entity):
     def __init__(self, game, ref, pos):
         super(Ball, self).__init__(game, pos.x, pos.y)
         self.ref = ref
+
+    def render(self):
+        surface = pg.Surface((16, 16), pg.SRCALPHA)
+        surface.fill({
+            1: (255, 0, 0),
+            2: (0, 255, 0),
+            3: (0, 0, 255),
+            4: (235, 0, 235),
+            5: (235, 235, 0)
+        }[self.ref.color])
+        return surface
