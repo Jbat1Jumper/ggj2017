4cc7aee4f8fe699f5c99af9e29bb2bf0cd2c6b1e
Seva Gavrilov <gavrilovseva@gmail.com>
2017-01-21 18:04:55 -0300

    Added score and checking of win condition.

--- ggj/model/__init__.py
+++ ggj/model/__init__.py
@@ -4,10 +4,11 @@ import random
 from .scene import Scene
 from .player import Player
 from .sniffer import Sniffer
-from .objectives import PatternObjectivesGenerator
+from .objectives import PatternObjectivesGenerator, Score
 
 NUMBER_OF_OBJECTIVES = 3
 PATTERN_LENGTH = 3
+MAX_SCORE = 9
 
 
 class Model(object):
@@ -18,6 +19,7 @@ class Model(object):
         self.scene = Scene()
         self.left_player = Player()
         self.right_player = Player()
+        self.score = Score(MAX_SCORE)
 
         number, length = NUMBER_OF_OBJECTIVES, PATTERN_LENGTH
         self.objectives_gen = PatternObjectivesGenerator(self, number, length)
@@ -64,12 +66,14 @@ class Model(object):
     def switch_player(self):
         self.currently_left = not self.currently_left
 
-    def check_conditions(self):
-        result = (False, False)
+    def check_objectives(self):
+        result = [False, False]
         for obj in self.objectives:
             if obj.check_conditions(self.scene.left_tube):
+                self.score.add_to_left(obj)
                 result[0] = True
             if obj.check_conditions(self.scene.right_tube):
+                self.score.add_to_right(obj)
                 result[1] = True
             if any(result):
                 self.objectives_gen.replace_objective(obj)
@@ -77,7 +81,7 @@ class Model(object):
         return result
 
     def check_win_conditions(self):
-        return self.objective.check_conditions()
+        return self.score.check_win()
 
     def __str__(self):
         s = 'Conditions: ' + ' | '.join(str(x) for x in self.objectives) + '\n\n'
